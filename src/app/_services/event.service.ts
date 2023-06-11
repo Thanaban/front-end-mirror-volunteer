d60a1f7097e94668f6c2a59f147efa60a79f699d
Thanaban <s6001012610038@email.kmutnb.ac.th>
2023-06-11 14:56:59 +0700

    event api

--- src/app/_services/event.service.ts
+++ src/app/_services/event.service.ts
@@ -2,226 +2,225 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-};
-
-const ACTIVITY_API = 'https://backend-volunteer.onrender.com/activities/';
-
 @Injectable({
   providedIn: 'root',
 })
 export class EventService {
+  private readonly AUTH_TOKEN_KEY = 'jwtToken'; // Update with your token key
+
   constructor(private http: HttpClient) {}
 
+  private getJwtToken(): string {
+    return localStorage.getItem(this.AUTH_TOKEN_KEY) || '';
+  }
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + this.getJwtToken()
+    });
+  }
+
   get_open_activity(): Observable<any> {
-    return this.http.get(ACTIVITY_API + 'open_activity', httpOptions);
+    const headers = this.getHeaders();
+    return this.http.get('https://backend-volunteer.onrender.com/activities/open_activity', { headers });
   }
 
-  join_activity(
-    activityId: number,
-    userId: number,
-    date: Date
-  ): Observable<any> {
-    return this.http.post(
-      'https://backend-volunteer.onrender.com/users/dateAc2',
-      {
-        activityId,
-        userId,
-        date,
-      },
-      httpOptions
-    );
+  join_activity(activityId: number, userId: number, date: Date): Observable<any> {
+    const headers = this.getHeaders();
+    const body = { activityId, userId, date };
+    return this.http.post('https://backend-volunteer.onrender.com/users/dateAc2', body, { headers });
   }
 
-  cancel_activity(
-    activityId: number,
-    userId: number,
-    date: Date
-  ): Observable<any> {
-    return this.http.patch(
-      'http://localhost:8000/users/cancel_activity',
-      {
-        activityId,
-        userId,
-        date,
-      },
-      httpOptions
-    );
-  }
 
-  status_activity(id: number, is_open: boolean): Observable<any> {
-    return this.http.patch(
-      'http://localhost:8000/activities/update_activity_status',
-      {
-        id,
-        is_open,
-      },
-      httpOptions
-    );
-  }
+  // cancel_activity(
+  //   activityId: number,
+  //   userId: number,
+  //   date: Date
+  // ): Observable<any> {
+  //   return this.http.patch(
+  //     'http://localhost:8000/users/cancel_activity',
+  //     {
+  //       activityId,
+  //       userId,
+  //       date,
+  //     },
+  //     httpOptions
+  //   );
+  // }
 
-  update_activity(
-    id: number,
-    activity_name: string,
-    time_detail: string,
-    clothes_detail: string,
-    map: string,
-    travel_public_detail: string,
-    travel_detail: string,
-    travel_etc_detail: string
-  ): Observable<any> {
-    return this.http.patch(
-      'http://localhost:8000/activities/update_activity',
-      {
-        id,
-        activity_name,
-        time_detail,
-        clothes_detail,
-        map,
-        travel_public_detail,
-        travel_detail,
-        travel_etc_detail,
-      },
-      httpOptions
-    );
-  }
+  // status_activity(id: number, is_open: boolean): Observable<any> {
+  //   return this.http.patch(
+  //     'http://localhost:8000/activities/update_activity_status',
+  //     {
+  //       id,
+  //       is_open,
+  //     },
+  //     httpOptions
+  //   );
+  // }
 
-  create_activity(
-    activity_name: string,
-    activity_details: string,
-    clothes_detail: string,
-    etc_detail: string,
-    time_detail: string,
-    timeline: string,
-    travel_detail: string,
-    travel_etc_detail: string,
-    travel_public_detail: string,
-    regised_number: number,
-    size_number: number,
-    received_hours: number,
-    map: string,
-    start_date: Date,
-    end_date: Date,
+  // update_activity(
+  //   id: number,
+  //   activity_name: string,
+  //   time_detail: string,
+  //   clothes_detail: string,
+  //   map: string,
+  //   travel_public_detail: string,
+  //   travel_detail: string,
+  //   travel_etc_detail: string
+  // ): Observable<any> {
+  //   return this.http.patch(
+  //     'http://localhost:8000/activities/update_activity',
+  //     {
+  //       id,
+  //       activity_name,
+  //       time_detail,
+  //       clothes_detail,
+  //       map,
+  //       travel_public_detail,
+  //       travel_detail,
+  //       travel_etc_detail,
+  //     },
+  //     httpOptions
+  //   );
+  // }
 
-    picture: string,
-    priority: number
-  ): Observable<any> {
-    return this.http.post(
-      'http://localhost:8000/activities/createActivity',
-      {
-        activity_name,
-        activity_details,
-        clothes_detail,
-        etc_detail,
-        time_detail,
-        timeline,
-        travel_detail,
-        travel_etc_detail,
-        travel_public_detail,
-        regised_number,
-        size_number,
-        received_hours,
-        map,
-        start_date,
-        end_date,
-        picture,
-        priority,
-      },
-      httpOptions
-    );
-  }
+  // create_activity(
+  //   activity_name: string,
+  //   activity_details: string,
+  //   clothes_detail: string,
+  //   etc_detail: string,
+  //   time_detail: string,
+  //   timeline: string,
+  //   travel_detail: string,
+  //   travel_etc_detail: string,
+  //   travel_public_detail: string,
+  //   regised_number: number,
+  //   size_number: number,
+  //   received_hours: number,
+  //   map: string,
+  //   start_date: Date,
+  //   end_date: Date,
 
-  status_user(id: number, non_blacklist: boolean): Observable<any> {
-    return this.http.patch(
-      'http://localhost:8000/activities/update_blacklist',
-      {
-        id,
-        non_blacklist,
-      },
-      httpOptions
-    );
-  }
+  //   picture: string,
+  //   priority: number
+  // ): Observable<any> {
+  //   return this.http.post(
+  //     'http://localhost:8000/activities/createActivity',
+  //     {
+  //       activity_name,
+  //       activity_details,
+  //       clothes_detail,
+  //       etc_detail,
+  //       time_detail,
+  //       timeline,
+  //       travel_detail,
+  //       travel_etc_detail,
+  //       travel_public_detail,
+  //       regised_number,
+  //       size_number,
+  //       received_hours,
+  //       map,
+  //       start_date,
+  //       end_date,
+  //       picture,
+  //       priority,
+  //     },
+  //     httpOptions
+  //   );
+  // }
 
-  post_comment(
-    userId: number,
-    userActivityId: number,
-    activityId: number,
+  // status_user(id: number, non_blacklist: boolean): Observable<any> {
+  //   return this.http.patch(
+  //     'http://localhost:8000/activities/update_blacklist',
+  //     {
+  //       id,
+  //       non_blacklist,
+  //     },
+  //     httpOptions
+  //   );
+  // }
 
-    comment_detail: string
-  ): Observable<any> {
-    return this.http.post(
-      'http://localhost:8000/users/post_comment',
-      {
-        userId,
-        userActivityId,
-        activityId,
-        comment_detail,
-      },
-      httpOptions
-    );
-  }
+  // post_comment(
+  //   userId: number,
+  //   userActivityId: number,
+  //   activityId: number,
 
-  get_user_in_userAc(id: number): Observable<any> {
-    return this.http.post(
-      'http://localhost:8000/activities/get_user_in_userAc',
-      {
-        id,
-      }
-    );
-  }
+  //   comment_detail: string
+  // ): Observable<any> {
+  //   return this.http.post(
+  //     'http://localhost:8000/users/post_comment',
+  //     {
+  //       userId,
+  //       userActivityId,
+  //       activityId,
+  //       comment_detail,
+  //     },
+  //     httpOptions
+  //   );
+  // }
 
-  remove_user_from_useractivity(
-    userId: number,
-    userActivityId: number
-  ): Observable<any> {
-    return this.http.patch(
-      'http://localhost:8000/activities/remove_user_from_useractivity',
-      {
-        userId,
-        userActivityId,
-      },
-      httpOptions
-    );
-  }
+  // get_user_in_userAc(id: number): Observable<any> {
+  //   return this.http.post(
+  //     'http://localhost:8000/activities/get_user_in_userAc',
+  //     {
+  //       id,
+  //     }
+  //   );
+  // }
 
-  finish_activity(id: number): Observable<any> {
-    return this.http.patch(
-      'http://localhost:8000/activities/finish_activity',
-      {
-        id,
-      },
-      httpOptions
-    );
-  }
+  // remove_user_from_useractivity(
+  //   userId: number,
+  //   userActivityId: number
+  // ): Observable<any> {
+  //   return this.http.patch(
+  //     'http://localhost:8000/activities/remove_user_from_useractivity',
+  //     {
+  //       userId,
+  //       userActivityId,
+  //     },
+  //     httpOptions
+  //   );
+  // }
 
-  get_useractivity_by_id(id: number): Observable<any> {
-    return this.http.post(
-      'http://localhost:8000/activities/get_useractivity_by_id',
-      {
-        id,
-      },
-      httpOptions
-    );
-  }
+  // finish_activity(id: number): Observable<any> {
+  //   return this.http.patch(
+  //     'http://localhost:8000/activities/finish_activity',
+  //     {
+  //       id,
+  //     },
+  //     httpOptions
+  //   );
+  // }
 
-  get_commnet_form_Ac(activityId: number): Observable<any> {
-    return this.http.post(
-      'http://localhost:8000/activities/get_commnet_form_UserAc',
-      {
-        activityId,
-      },
-      httpOptions
-    );
-  }
+  // get_useractivity_by_id(id: number): Observable<any> {
+  //   return this.http.post(
+  //     'http://localhost:8000/activities/get_useractivity_by_id',
+  //     {
+  //       id,
+  //     },
+  //     httpOptions
+  //   );
+  // }
 
-  get_one_activity(activityId: number): Observable<any> {
-    return this.http.post(
-      'http://localhost:8000/users/get_one_activity',
-      {
-        activityId,
-      },
-      httpOptions
-    );
-  }
+  // get_commnet_form_Ac(activityId: number): Observable<any> {
+  //   return this.http.post(
+  //     'http://localhost:8000/activities/get_commnet_form_UserAc',
+  //     {
+  //       activityId,
+  //     },
+  //     httpOptions
+  //   );
+  // }
+
+  // get_one_activity(activityId: number): Observable<any> {
+  //   return this.http.post(
+  //     'http://localhost:8000/users/get_one_activity',
+  //     {
+  //       activityId,
+  //     },
+  //     httpOptions
+  //   );
+  // }
 }
