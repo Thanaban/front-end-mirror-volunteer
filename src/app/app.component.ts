43b1ca3095df672d95969ec1e572ddbf9c2cd1e5
Thanaban <s6001012610038@email.kmutnb.ac.th>
2023-06-11 13:39:30 +0700

    test user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,19 +54,19 @@ export class AppComponent {
 
       this.currentUser = this.storageService.getUser();
       console.warn(this.currentUser)
-      this.http
-        .get('https://backend-volunteer.onrender.com/users/user')
-        .subscribe((response) => {
-          this.currentUser = response;
-          console.warn('result', this.currentUser);
-          if (this.currentUser.admin) {
-            this.isAdmin = this.currentUser.admin;
-            console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', this.isAdmin);
-          } else {
-            this.isAdmin = this.currentUser.admin;
-            console.warn('BBBBBBBBBBBBBBBBBBBBBBB', this.isAdmin);
-          }
-        });
+      // this.http
+      //   .get('https://backend-volunteer.onrender.com/users/user')
+      //   .subscribe((response) => {
+      //     this.currentUser = response;
+      //     console.warn('result', this.currentUser);
+      //     if (this.currentUser.admin) {
+      //       this.isAdmin = this.currentUser.admin;
+      //       console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', this.isAdmin);
+      //     } else {
+      //       this.isAdmin = this.currentUser.admin;
+      //       console.warn('BBBBBBBBBBBBBBBBBBBBBBB', this.isAdmin);
+      //     }
+      //   });
       
       this.http
         .get(
