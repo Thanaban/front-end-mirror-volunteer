bbb7f9a87b160c6af6c3670a92f534f26c4ec19f
Thanaban <s6001012610038@email.kmutnb.ac.th>
2023-06-14 00:19:59 +0700

    test

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:4200/',
+    baseUrl: 'https://front-end-mirror-volunteer.netlify.app',
   },
 });
