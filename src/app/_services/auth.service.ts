f1e4d2af2d8ad403f92a44face05da871ebbf62e
Thanaban <s6001012610038@email.kmutnb.ac.th>
2023-06-14 00:29:03 +0700

    same

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -4,92 +4,81 @@ import { Observable } from 'rxjs';
 
 const AUTH_API = 'https://volunteer-management-backend.onrender.com/users/';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  login(email: string, password: string): Observable<any> {
-    return this.http.post<any>(
+  login(email: string, password: string): Observable<any> { console.warn(httpOptions)
+    return this.http.post(
       AUTH_API + 'login',
-      { email, password },
-      { headers: this.getHeaders() }
+      {
+        email,
+        password,
+      },
+      httpOptions
     );
   }
 
-  private getHeaders(): HttpHeaders {
-    const headers = new HttpHeaders().set(
-      'Content-Type',
-      'application/json'
+  register(
+    email: string, 
+    password: string, 
+    name:string,
+    lastname:string,
+    nickname:string,
+    birthday: Date,
+    gender: string,
+    religion: string,
+    phoneNumber: string,
+    career: string,
+    workplace: string,
+    congenitalDisease: string,
+    allergicFood: string,
+    talent: string,
+    know_from: string
+    ): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'register',
+      {
+        email, 
+        password,
+        name, 
+        lastname, 
+        nickname, 
+        birthday, 
+        gender, 
+        religion, 
+        phoneNumber, 
+        career, 
+        workplace, 
+        congenitalDisease, 
+        allergicFood, 
+        talent, 
+        know_from
+      },
+      httpOptions
     );
-    const token = localStorage.getItem('jwtToken');
-    if (token) {
-      headers.set('Authorization', `Bearer ${token}`);
-    }
-    return headers;
   }
 
-  private storeToken(token: string): void {
-    // Store the token in localStorage or a secure cookie
-    // Example using localStorage:
-    localStorage.setItem('jwtToken', token);
+  emailResetPassWord(email:string): Observable<any> {
+    return this.http.post('http://localhost:8000/users/password-reset',{email},httpOptions);
   }
 
-  // register(
-  //   email: string, 
-  //   password: string, 
-  //   name:string,
-  //   lastname:string,
-  //   nickname:string,
-  //   birthday: Date,
-  //   gender: string,
-  //   religion: string,
-  //   phoneNumber: string,
-  //   career: string,
-  //   workplace: string,
-  //   congenitalDisease: string,
-  //   allergicFood: string,
-  //   talent: string,
-  //   know_from: string
-  //   ): Observable<any> {
-  //   return this.http.post(
-  //     AUTH_API + 'register',
-  //     {
-  //       email, 
-  //       password,
-  //       name, 
-  //       lastname, 
-  //       nickname, 
-  //       birthday, 
-  //       gender, 
-  //       religion, 
-  //       phoneNumber, 
-  //       career, 
-  //       workplace, 
-  //       congenitalDisease, 
-  //       allergicFood, 
-  //       talent, 
-  //       know_from
-  //     },
-  //     httpOptions
-  //   );
-  // }
+  confirmResetPassWord(password:string): Observable<any> {
+    return this.http.post('http://localhost:8000/users/reset-password',{password},httpOptions);
+  }
 
-  // emailResetPassWord(email:string): Observable<any> {
-  //   return this.http.post('http://localhost:8000/users/password-reset',{email},httpOptions);
-  // }
-
-  // confirmResetPassWord(password:string): Observable<any> {
-  //   return this.http.post('http://localhost:8000/users/reset-password',{password},httpOptions);
-  // }
-
-  // logout(): Observable<any> {
-  //   return this.http.post(AUTH_API + 'logout', { }, httpOptions);
+  logout(): Observable<any> {
+    return this.http.post(AUTH_API + 'logout', { }, httpOptions);
     
-  // }
+  }
 
-  // refreshToken() {
-  //   return this.http.post(AUTH_API + 'refreshtoken', { }, httpOptions);
-  // }
+  refreshToken() {
+    return this.http.post(AUTH_API + 'refreshtoken', { }, httpOptions);
+  }
 }
