477201ed67f6e5fc1f5ed37d2e8449def6839c93
Thanaban <s6001012610038@email.kmutnb.ac.th>
2023-06-15 23:03:37 +0700

    test

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -5,8 +5,10 @@ import { Observable } from 'rxjs';
 const AUTH_API = 'https://volunteer-management-backend.onrender.com/users/';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  withCredentials: true
 };
+
 
 @Injectable({
   providedIn: 'root',
