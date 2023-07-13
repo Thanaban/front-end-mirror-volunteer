897de6d6d8c84d3292a111de0ca2ec2125748836
Thanaban <s6001012610038@email.kmutnb.ac.th>
2023-07-14 03:41:42 +0700

    facebook-icon

--- src/app/profile/certificate/certificate.component.ts
+++ src/app/profile/certificate/certificate.component.ts
@@ -1,22 +1,21 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
-
 
 @Component({
   selector: 'app-certificate',
   templateUrl: './certificate.component.html',
-  styleUrls: ['./certificate.component.css']
+  styleUrls: ['./certificate.component.css'],
 })
 export class CertificateComponent {
   dateForm: any;
   month: any;
-  certi_data :any
+  certi_data: any;
 
   ngOnInit(): void {
     let data: any = localStorage.getItem('CERTI');
     this.certi_data = JSON.parse(data);
-    console.warn('cer' ,this.certi_data.currentUser.result.name)
+    console.warn('cer', this.certi_data.currentUser.result.name);
     const element = document.getElementById('element-to-export');
     window.scrollTo(0, 0);
     if (element?.nodeName) {
@@ -34,7 +33,7 @@ export class CertificateComponent {
       console.log('Element not found');
     }
   }
-  
+
   con_date(d: any) {
     d = d.split('-');
     this.month = d[1];
@@ -46,32 +45,24 @@ export class CertificateComponent {
       d[1] = 'มิ.ย';
     } else if (d[1] == '07') {
       d[1] = 'ก.ค';
-    }
-    else if (d[1] == '08') {
+    } else if (d[1] == '08') {
       d[1] = 'ส.ค';
-    }
-    else if (d[1] == '09') {
+    } else if (d[1] == '09') {
       d[1] = 'ก.ย';
-    }
-    else if (d[1] == '10') {
+    } else if (d[1] == '10') {
       d[1] = 'ต.ค';
-    }
-    else if (d[1] == '11') {
+    } else if (d[1] == '11') {
       d[1] = 'พ.ย';
-    }
-    else if (d[1] == '12') {
+    } else if (d[1] == '12') {
       d[1] = 'ธ.ค';
-    }
-    else if (d[1] == '01') {
+    } else if (d[1] == '01') {
       d[1] = 'ม.ค';
-    }
-    else if (d[1] == '02') {
+    } else if (d[1] == '02') {
       d[1] = 'ก.พ';
-    }
-    else if (d[1] == '03') {
+    } else if (d[1] == '03') {
       d[1] = 'มี.ค';
     }
-    
+
     d[0] = parseInt(d[0]) + 543;
     this.dateForm = d.reverse().join(' ');
     return this.dateForm;
