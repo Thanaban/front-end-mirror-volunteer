11aea7cc8ca1dea9061f7c50510a229c92ad0fdf
Thanaban <s6001012610038@email.kmutnb.ac.th>
2023-07-13 20:37:17 +0700

    userId.length

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,7 +77,7 @@ export class AppComponent {
           this.eventTomorrow = data;
           console.warn(data);
           for (let i = 0; i < this.eventTomorrow.length; i++) {
-            console.warn('i lenght', this.eventTomorrow.length);
+            console.warn('i lenght', this.eventTomorrow[i].userId.length);
             console.warn('i', i);
             for (let j = 0; j < this.eventTomorrow[i].userId.length; j++) {
               console.warn('j', j);
