567d8eaee90a3561c9ba45ffa8ad25d30a098de6
Thanaban <s6001012610038@email.kmutnb.ac.th>
2023-07-14 23:10:07 +0700

    test cer

--- src/app/profile/certificate/certificate.component.ts
+++ src/app/profile/certificate/certificate.component.ts
@@ -19,121 +19,121 @@ export class CertificateComponent {
     let data: any = localStorage.getItem('CERTI');
     this.certi_data = JSON.parse(data);
     console.warn('cer', this.certi_data);
-    // const element = document.getElementById('element-to-export');
-    // window.scrollTo(0, 0);
-    // if (element?.nodeName) {
-    //   html2canvas(element).then((canvas) => {
-    //     const doc = new jsPDF();
-    //     const imgData = canvas.toDataURL('image/png');
-    //     const imgProps = doc.getImageProperties(imgData);
-    //     const pdfWidth = doc.internal.pageSize.getWidth();
-    //     const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    const element = document.getElementById('element-to-export');
+    window.scrollTo(0, 0);
+    if (element?.nodeName) {
+      html2canvas(element).then((canvas) => {
+        const doc = new jsPDF();
+        const imgData = canvas.toDataURL('image/png');
+        const imgProps = doc.getImageProperties(imgData);
+        const pdfWidth = doc.internal.pageSize.getWidth();
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
 
-    //     doc.addImage(imgData, 'PNG', 0, -10, pdfWidth, pdfHeight);
-    //     doc.save('certificate.pdf');
-    //   });
-    // } else {
-    //   console.log('Element not found');
-    // }
-    this.generatePDF();
-  }
-
-  async generatePDF() {
-    const html = `<html>
-    <body id="element-to-export">
-      <!-- <div class="container" > -->
-      <div class="top-padding"></div>
-      <div class="header">" อาสาสมัคร "</div>
-  
-      <div class="tagline">
-        คือคนที่มีหัวใจอาสา คนที่อยากเห็นสังคมงดงาม<br />
-        คนที่แสวงหาความหมายของการมีชีวิตอยู่อย่างมีคุณค่า
-      </div>
-  
-      <div class="ensure">
-        มูลนิธิกระจกเงา ขอรับรองว่า {{ certi_data?.currentUser?.result?.name }}
-        {{ certi_data?.currentUser?.result?.lastname }}<br />
-        ได้เข้าร่วมเป็นอาสาสมัครในกิจกรรม
-        {{ certi_data?.currentActivityName?.getEvent?.activity_name }}<br />
-        ชื่อกิจกรรม {{ certi_data?.currentActivityName?.getEvent?.activity_name
-        }}<br />
-        เป็นเวลา 8 ชั่วโมง
-      </div>
-  
-      <div class="content">
-        ระหว่างการทำกิจกรรมอาสาสมัครได้ปฏิบัติหน้าที่ด้วยความเสียสละ มีน้ำใจ
-        และให้ความ<br />
-        ร่วมมือแสดงความมีส่วนร่วมที่สำคัญต่อกระบวนการอาสาสมัครหนุนเสริมเป็นกลไกการขับเคลื่อนสังคม<br />
-        ร่วมกับมูลนิธิกระจกเงาได้เป็นอย่างดี
-      </div>
-  
-      <div class="content">
-        มูลนิธิกระจกเงา
-        ขอขอบคุณท่านที่ได้ร่วมเป็นส่วนหนึ่งในกิจกรรมอาสาสมัครในครั้งนี้ <br />
-        ขอให้บทบาทในการเป็น “อาสาสมัคร” ในตัวท่าน
-        ได้ต่อยอดเพื่อสร้างประโยชน์ต่อสังคมสืบไป
-      </div>
-  
-      <div class="content">
-        ออกให้ ณ วันที่ {{ con_date(certi_data.currentActivityName.date) }}
-      </div>
-  
-      <div class="marquee">Certificate of Arsa</div>
-  
-      <div class="assignment">This certificate is presented to</div>
-  
-      <div class="person">Pan</div>
-  
-      <div class="reason">
-        วันที่<br />
-        123181
-      </div>
-      <div class="bottom-padding"></div>
-      <!-- </div> -->
-    </body>
-  </html>
-  `;
-
-    try {
-      const pdfDoc = await PDFDocument.create();
-      const fontBytes = '<<BASE64_FONT_DATA>>'; // Replace with the actual base64-encoded font data
-      const customFont = await pdfDoc.embedFont(fontBytes);
-
-      const page = pdfDoc.addPage();
-      page.setFont(customFont);
-      page.setFontSize(12);
-
-      const fontColor = rgb(0, 0, 0);
-      const textWidth = page.getWidth() - 40;
-      const textHeight = page.getHeight() - 40;
-
-      const lines = html.split('\n');
-      const lineHeight = 20;
-      const numLines = lines.length;
-      const calculatedHeight = numLines * lineHeight;
-      const adjustedTextHeight = textHeight - calculatedHeight;
-
-      lines.forEach((line, index) => {
-        const y = adjustedTextHeight + index * lineHeight;
-        page.drawText(line, {
-          x: 20,
-          y,
-          maxWidth: textWidth,
-          lineHeight,
-          size: 12,
-          color: fontColor,
-        });
+        doc.addImage(imgData, 'PNG', 0, -10, pdfWidth, pdfHeight);
+        doc.save('certificate.pdf');
       });
-
-      const pdfBytes = await pdfDoc.save();
-      const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-      saveAs(blob, 'certificate.pdf');
-
-      console.log('PDF generated successfully!');
-    } catch (error) {
-      console.error('Error generating PDF:', error);
+    } else {
+      console.log('Element not found');
     }
+    // this.generatePDF();
   }
+
+  // async generatePDF() {
+  //   const html = `<html>
+  //   <body id="element-to-export">
+  //     <!-- <div class="container" > -->
+  //     <div class="top-padding"></div>
+  //     <div class="header">" อาสาสมัคร "</div>
+  
+  //     <div class="tagline">
+  //       คือคนที่มีหัวใจอาสา คนที่อยากเห็นสังคมงดงาม<br />
+  //       คนที่แสวงหาความหมายของการมีชีวิตอยู่อย่างมีคุณค่า
+  //     </div>
+  
+  //     <div class="ensure">
+  //       มูลนิธิกระจกเงา ขอรับรองว่า {{ certi_data?.currentUser?.result?.name }}
+  //       {{ certi_data?.currentUser?.result?.lastname }}<br />
+  //       ได้เข้าร่วมเป็นอาสาสมัครในกิจกรรม
+  //       {{ certi_data?.currentActivityName?.getEvent?.activity_name }}<br />
+  //       ชื่อกิจกรรม {{ certi_data?.currentActivityName?.getEvent?.activity_name
+  //       }}<br />
+  //       เป็นเวลา 8 ชั่วโมง
+  //     </div>
+  
+  //     <div class="content">
+  //       ระหว่างการทำกิจกรรมอาสาสมัครได้ปฏิบัติหน้าที่ด้วยความเสียสละ มีน้ำใจ
+  //       และให้ความ<br />
+  //       ร่วมมือแสดงความมีส่วนร่วมที่สำคัญต่อกระบวนการอาสาสมัครหนุนเสริมเป็นกลไกการขับเคลื่อนสังคม<br />
+  //       ร่วมกับมูลนิธิกระจกเงาได้เป็นอย่างดี
+  //     </div>
+  
+  //     <div class="content">
+  //       มูลนิธิกระจกเงา
+  //       ขอขอบคุณท่านที่ได้ร่วมเป็นส่วนหนึ่งในกิจกรรมอาสาสมัครในครั้งนี้ <br />
+  //       ขอให้บทบาทในการเป็น “อาสาสมัคร” ในตัวท่าน
+  //       ได้ต่อยอดเพื่อสร้างประโยชน์ต่อสังคมสืบไป
+  //     </div>
+  
+  //     <div class="content">
+  //       ออกให้ ณ วันที่ {{ con_date(certi_data.currentActivityName.date) }}
+  //     </div>
+  
+  //     <div class="marquee">Certificate of Arsa</div>
+  
+  //     <div class="assignment">This certificate is presented to</div>
+  
+  //     <div class="person">Pan</div>
+  
+  //     <div class="reason">
+  //       วันที่<br />
+  //       123181
+  //     </div>
+  //     <div class="bottom-padding"></div>
+  //     <!-- </div> -->
+  //   </body>
+  // </html>
+  // `;
+
+  //   try {
+  //     const pdfDoc = await PDFDocument.create();
+  //     const fontBytes = '<<BASE64_FONT_DATA>>'; // Replace with the actual base64-encoded font data
+  //     const customFont = await pdfDoc.embedFont(fontBytes);
+
+  //     const page = pdfDoc.addPage();
+  //     page.setFont(customFont);
+  //     page.setFontSize(12);
+
+  //     const fontColor = rgb(0, 0, 0);
+  //     const textWidth = page.getWidth() - 40;
+  //     const textHeight = page.getHeight() - 40;
+
+  //     const lines = html.split('\n');
+  //     const lineHeight = 20;
+  //     const numLines = lines.length;
+  //     const calculatedHeight = numLines * lineHeight;
+  //     const adjustedTextHeight = textHeight - calculatedHeight;
+
+  //     lines.forEach((line, index) => {
+  //       const y = adjustedTextHeight + index * lineHeight;
+  //       page.drawText(line, {
+  //         x: 20,
+  //         y,
+  //         maxWidth: textWidth,
+  //         lineHeight,
+  //         size: 12,
+  //         color: fontColor,
+  //       });
+  //     });
+
+  //     const pdfBytes = await pdfDoc.save();
+  //     const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+  //     saveAs(blob, 'certificate.pdf');
+
+  //     console.log('PDF generated successfully!');
+  //   } catch (error) {
+  //     console.error('Error generating PDF:', error);
+  //   }
+  // }
 
   con_date(d: any) {
     d = d.split('-');
