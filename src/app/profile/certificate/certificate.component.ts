43d4c170755b262bdb6f0dd1ec8bdbccc7ff85e2
Thanaban <s6001012610038@email.kmutnb.ac.th>
2023-07-14 21:57:39 +0700

    generatePDF2

--- src/app/profile/certificate/certificate.component.ts
+++ src/app/profile/certificate/certificate.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
+import * as puppeteer from 'puppeteer';
 
 @Component({
   selector: 'app-certificate',
@@ -13,26 +14,102 @@ export class CertificateComponent {
   certi_data: any;
 
   ngOnInit(): void {
-    let data: any = localStorage.getItem('CERTI');
-    this.certi_data = JSON.parse(data);
-    console.warn('cer', this.certi_data.currentUser.result.name);
-    const element = document.getElementById('element-to-export');
-    window.scrollTo(0, 0);
-    if (element?.nodeName) {
-      html2canvas(element).then((canvas) => {
-        const doc = new jsPDF();
-        const imgData = canvas.toDataURL('image/png');
-        const imgProps = doc.getImageProperties(imgData);
-        const pdfWidth = doc.internal.pageSize.getWidth();
-        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    // let data: any = localStorage.getItem('CERTI');
+    // this.certi_data = JSON.parse(data);
+    // console.warn('cer', this.certi_data.currentUser.result.name);
+    // const element = document.getElementById('element-to-export');
+    // window.scrollTo(0, 0);
+    // if (element?.nodeName) {
+    //   html2canvas(element).then((canvas) => {
+    //     const doc = new jsPDF();
+    //     const imgData = canvas.toDataURL('image/png');
+    //     const imgProps = doc.getImageProperties(imgData);
+    //     const pdfWidth = doc.internal.pageSize.getWidth();
+    //     const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
 
-        doc.addImage(imgData, 'PNG', 0, -10, pdfWidth, pdfHeight);
-        doc.save('certificate.pdf');
-      });
-    } else {
-      console.log('Element not found');
+    //     doc.addImage(imgData, 'PNG', 0, -10, pdfWidth, pdfHeight);
+    //     doc.save('certificate.pdf');
+    //   });
+    // } else {
+    //   console.log('Element not found');
+    // }
+    this.generatePDF2()
+  }
+
+  async generatePDF2() {
+    const html = `<html>
+    <body id="element-to-export">
+      <!-- <div class="container" > -->
+      <div class="top-padding"></div>
+      <div class="header">" อาสาสมัคร "</div>
+  
+      <div class="tagline">
+        คือคนที่มีหัวใจอาสา คนที่อยากเห็นสังคมงดงาม<br />
+        คนที่แสวงหาความหมายของการมีชีวิตอยู่อย่างมีคุณค่า
+      </div>
+  
+      <div class="ensure">
+        มูลนิธิกระจกเงา ขอรับรองว่า {{ certi_data?.currentUser?.result?.name }}
+        {{ certi_data?.currentUser?.result?.lastname }}<br />
+        ได้เข้าร่วมเป็นอาสาสมัครในกิจกรรม
+        {{ certi_data?.currentActivityName?.getEvent?.activity_name }}<br />
+        ชื่อกิจกรรม {{ certi_data?.currentActivityName?.getEvent?.activity_name
+        }}<br />
+        เป็นเวลา 8 ชั่วโมง
+      </div>
+  
+      <div class="content">
+        ระหว่างการทำกิจกรรมอาสาสมัครได้ปฏิบัติหน้าที่ด้วยความเสียสละ มีน้ำใจ
+        และให้ความ<br />
+        ร่วมมือแสดงความมีส่วนร่วมที่สำคัญต่อกระบวนการอาสาสมัครหนุนเสริมเป็นกลไกการขับเคลื่อนสังคม<br />
+        ร่วมกับมูลนิธิกระจกเงาได้เป็นอย่างดี
+      </div>
+  
+      <div class="content">
+        มูลนิธิกระจกเงา
+        ขอขอบคุณท่านที่ได้ร่วมเป็นส่วนหนึ่งในกิจกรรมอาสาสมัครในครั้งนี้ <br />
+        ขอให้บทบาทในการเป็น “อาสาสมัคร” ในตัวท่าน
+        ได้ต่อยอดเพื่อสร้างประโยชน์ต่อสังคมสืบไป
+      </div>
+  
+      <div class="content">
+        ออกให้ ณ วันที่ {{ con_date(certi_data.currentActivityName.date) }}
+      </div>
+  
+      <div class="marquee">Certificate of Arsa</div>
+  
+      <div class="assignment">This certificate is presented to</div>
+  
+      <div class="person">Pan</div>
+  
+      <div class="reason">
+        วันที่<br />
+        123181
+      </div>
+      <div class="bottom-padding"></div>
+      <!-- </div> -->
+    </body>
+  </html>
+  `;
+
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      // Set the HTML content
+      await page.setContent(html, { waitUntil: 'networkidle0' });
+
+      // Generate PDF
+      await page.pdf({ path: 'output.pdf', format: 'A4' });
+
+      await browser.close();
+
+      console.log('PDF generated successfully!');
+    } catch (error) {
+      console.error('Error generating PDF:', error);
     }
   }
+  
 
   con_date(d: any) {
     d = d.split('-');
