e219d036a8d4fce68ebbeb0024160c8c941393aa
Thanaban <s6001012610038@email.kmutnb.ac.th>
2023-07-15 03:56:56 +0700

    date and font

--- src/app/profile/certificate/certificate.component.ts
+++ src/app/profile/certificate/certificate.component.ts
@@ -10,6 +10,9 @@ import html2canvas from 'html2canvas';
   styleUrls: ['./certificate.component.css'],
 })
 export class CertificateComponent implements OnInit {
+
+  dateForm: any;
+  month: any;
   certi_data: any;
   test:string = 'หกาสหาดว้า'
   @ViewChild('elementToExport', { static: true }) elementToExport!: ElementRef;
@@ -40,6 +43,40 @@ export class CertificateComponent implements OnInit {
       const blob = new Blob([pdfBytes], { type: 'application/pdf' });
       saveAs(blob, 'angular-demo.pdf');
     });
+  }
+
+  con_date(d: any) {
+    d = d.split('-');
+    this.month = d[1];
+    if (d[1] == '04') {
+      d[1] = 'เม.ย';
+    } else if (d[1] == '05') {
+      d[1] = 'พ.ค';
+    } else if (d[1] == '06') {
+      d[1] = 'มิ.ย';
+    } else if (d[1] == '07') {
+      d[1] = 'ก.ค';
+    } else if (d[1] == '08') {
+      d[1] = 'ส.ค';
+    } else if (d[1] == '09') {
+      d[1] = 'ก.ย';
+    } else if (d[1] == '10') {
+      d[1] = 'ต.ค';
+    } else if (d[1] == '11') {
+      d[1] = 'พ.ย';
+    } else if (d[1] == '12') {
+      d[1] = 'ธ.ค';
+    } else if (d[1] == '01') {
+      d[1] = 'ม.ค';
+    } else if (d[1] == '02') {
+      d[1] = 'ก.พ';
+    } else if (d[1] == '03') {
+      d[1] = 'มี.ค';
+    }
+
+    d[0] = parseInt(d[0]) + 543;
+    this.dateForm = d.reverse().join(' ');
+    return this.dateForm;
   }
 }
   // async generatePDF(): Promise<void> {
