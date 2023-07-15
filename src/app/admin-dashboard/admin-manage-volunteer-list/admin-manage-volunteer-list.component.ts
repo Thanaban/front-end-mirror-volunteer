3282baea6c1728042b0d4afabbc36905290a3577
Thanaban <s6001012610038@email.kmutnb.ac.th>
2023-07-15 21:13:40 +0700

    age

--- src/app/admin-dashboard/admin-manage-volunteer-list/admin-manage-volunteer-list.component.ts
+++ src/app/admin-dashboard/admin-manage-volunteer-list/admin-manage-volunteer-list.component.ts
@@ -100,4 +100,18 @@ export class AdminManageVolunteerListComponent implements OnInit {
         },
       });
   }
+
+  calculateAge(birthdate: string): number {
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDate.getDate())
+    ) {
+      age--;
+    }
+    return age;
+  }
 }
